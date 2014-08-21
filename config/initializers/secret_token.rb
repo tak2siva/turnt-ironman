43a522299faefaeff9dee46641795cb650630f73
Sivakumar <sivakumar@visolve.com>
2014-08-21 17:27:31 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Angular::Application.config.secret_token = '1987dc4a58a0e8a0150d794a4a83d66380d96a541b37a0eae53d60b1545ea550a0601a4ee9ed958b72139ec594eea2569afe4c7ad196395266ade5a26c6db556'
